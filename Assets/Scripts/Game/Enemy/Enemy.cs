8c2fbc888df895b9f38576e961122358a2eeec49
1817480 <a.rosagonzalez@wlv.ac.uk>
2018-11-20 16:35:11 +0000

    Worsheet 2

--- Assets/Scripts/Game/Enemy/Enemy.cs
+++ Assets/Scripts/Game/Enemy/Enemy.cs
@@ -9,7 +9,12 @@ public class Enemy : MonoBehaviour {
   public int pathIndex = 0;
 
   private int wayPointIndex = 0;
-  
+
+  void Start()
+  {
+    EnemyManager.Instance.RegisterEnemy(this);
+  }
+
   void OnGotToLastWayPoint() {
     Die();
   }
@@ -25,7 +30,46 @@ public class Enemy : MonoBehaviour {
 
   void Die() {
     if (gameObject != null) {
-      Destroy(gameObject);
+      //1
+      EnemyManager.Instance.UnRegister(this);
+      //2
+      gameObject.AddComponent<AutoScaler>().scaleSpeed = -2;
+      //3
+      enabled = false;
+      //4
+      Destroy(gameObject, 0.3f);
     }
   }
+
+  // ADDED
+  void Update()
+  {
+    //1
+    if (wayPointIndex < WayPointManager.Instance.Paths[pathIndex].WayPoints.Count)
+    {
+      UpdateMovement();
+    }
+    else
+    {//2
+      OnGotToLastWayPoint();
+    }
+  }
+
+  private void UpdateMovement()
+  {
+    //3
+    Vector3 targetPosition = WayPointManager.Instance.Paths[pathIndex].WayPoints[wayPointIndex].position;
+    //4
+    transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+    //5
+    transform.localRotation = UtilityMethods.SmoothlyLook(transform, targetPosition);
+    //6
+    if (Vector3.Distance(transform.position, targetPosition) < .1f)
+    {
+      wayPointIndex++;
+    }
+
+  }
 }
+
+
--- Assets/Scripts/Utility/UtilityMethods.cs
+++ Assets/Scripts/Utility/UtilityMethods.cs
@@ -1,9 +1,28 @@
 ﻿using UnityEngine;
 
-public static class UtilityMethods {
-  public static void MoveUiElementToWorldPosition(RectTransform rectTransform, Vector3 worldPosition) {
+public static class UtilityMethods
+{
+  public static void MoveUiElementToWorldPosition(RectTransform rectTransform, Vector3 worldPosition)
+  {
     Vector2 screenPoint = Camera.main.WorldToScreenPoint(worldPosition);
     rectTransform.position = screenPoint;
+  }
+
+  // 1
+  public static Quaternion SmoothlyLook (Transform fromTransform, Vector3 toVector3)
+  {
+    //2
+    if (fromTransform.position == toVector3)
+    {
+      return fromTransform.localRotation;
+    }
+
+    //3
+    Quaternion currentRotation = fromTransform.localRotation;
+    Quaternion targetRotation = Quaternion.LookRotation(toVector3 - fromTransform.position);
+
+    //4
+    return Quaternion.Slerp(currentRotation, targetRotation, Time.deltaTime * 10f);
   }
 }
 
