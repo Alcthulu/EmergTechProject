47ff40f657c3ccd220054b9b81779da3d34d53af
1817480 <a.rosagonzalez@wlv.ac.uk>
2018-11-20 19:24:03 +0000

    Worsheet 4

---
+++ Assets/Scripts/Game/UI/AddTowerWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTowerWindow : MonoBehaviour
+{
+  //1
+  public GameObject towerSlotToAddTowerTo;
+  //2
+  public void AddTower(string towerTypeAsString)
+  {
+    //3
+    TowerType type = (TowerType)Enum.Parse(typeof(TowerType), towerTypeAsString, true);
+    //4
+    if (TowerManager.Instance.GetTowerPrice(type) <= GameManager.Instance.gold)
+    {
+      //5
+      GameManager.Instance.gold -= TowerManager.Instance.GetTowerPrice(type);
+      gameObject.SetActive(false);
+    }
+  }
+
+}
